e4c6ca24ee26016c2cf71f3cb4aac31f9b9b4543
Gabriel Macario <moreirag1212@gmail.com>
2025-01-29 01:30:30 -0300

    commit changes

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once("config/url.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de contatos</title>
+</head>
+<body>
+    
+</body>
+</html>
